ce08dca04f02a56b47a70a1c27e6c833402064c4
CPettifo <cpettifo@myune.edu.au>
2024-11-15 18:07:23 +0100

    add unaccented word for test

--- src/pages/Motle.js
+++ src/pages/Motle.js
@@ -1,41 +1,43 @@
 import React, { useState, useEffect } from "react";
 
 const Motle = () => {
-  const [randomWord, setRandomWord] = useState("");
-  const [error, setError] = useState(null);
+    const [randomWord, setRandomWord] = useState("");
+    const [unaccentedWord, setUnaccentedWord] = useState("");
+    const [error, setError] = useState(null);
 
   // Fetch random word from the backend
-  useEffect(() => {
-    const fetchRandomWord = async () => {
-      try {
-        const response = await fetch(
-          "https://superb-bombolone-33f34b.netlify.app/.netlify/functions/fetchRandomWord"
-        );
+    useEffect(() => {
+        const fetchRandomWord = async () => {
+        try {
+            const response = await fetch(
+            "https://superb-bombolone-33f34b.netlify.app/.netlify/functions/fetchRandomWord"
+            );
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch the word");
+            if (!response.ok) {
+            throw new Error("Failed to fetch the word");
+            }
+
+            const data = await response.json();
+            setRandomWord(data.mot); 
+            setUnaccentedWord(data.unaccent);
+        } catch (err) {
+            setError(err.message);
         }
+        };
 
-        const data = await response.json();
-        setRandomWord(data.mot); 
-      } catch (err) {
-        setError(err.message);
-      }
+        fetchRandomWord();
+    }, []);
+
+    return (
+        <div className="motle">
+        <h1>Motle</h1>
+        {error ? (
+            <p style={{ color: "red" }}>Error: {error}</p>
+        ) : (
+            <p>The word is: {randomWord}, Unaccented it is: {unaccentedWord}</p>
+        )}
+        </div>
+    );
     };
 
-    fetchRandomWord();
-  }, []);
-
-  return (
-    <div className="motle">
-      <h1>Motle</h1>
-      {error ? (
-        <p style={{ color: "red" }}>Error: {error}</p>
-      ) : (
-        <p>The word is: {randomWord}</p>
-      )}
-    </div>
-  );
-};
-
-export default Motle;
+    export default Motle;
