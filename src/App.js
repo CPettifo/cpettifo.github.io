c8e3864f1e2964eee829e3400e30380f4ffc58e3
CPettifo <cpettifo@myune.edu.au>
2024-11-15 11:55:40 +0100

    port etch-a-sketch to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  // Default grid size
+  const [size, setSize] = useState(16); 
+  const boardRef = useRef(null);
+
+  const drawBoard = (size) => {
+    const board = boardRef.current;
+    // Clears the board
+    board.innerHTML = ""; 
+
+    for (let i = 0; i < size; i++) {
+      const rows = document.createElement("div");
+      rows.classList.add("row");
+
+      for (let j = 0; j < size; j++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.backgroundColor = "white";
+
+        pixel.addEventListener("mouseover", () => {
+          pixel.style.backgroundColor = "black";
+        });
+
+        pixel.addEventListener("touchmove", () => {
+          pixel.style.backgroundColor = "black";
+        });
+
+        rows.appendChild(pixel);
+      }
+      board.appendChild(rows);
+    }
+  };
+
+  useEffect(() => {
+    drawBoard(size);
+  }, [size]); 
+  // Re-render the board when size changes
+
+  const resetBoard = () => {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+      pixel.style.backgroundColor = "white";
+    });
+  };
+
+  const handleSliderChange = (event) => {
+    const newSize = event.target.value;
+    setSize(parseInt(newSize, 10));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="etch-a-sketch">
+      <h1>Hey there</h1>
+      <h2>Portfolio in progress, Etch-a-Sketch while we wait.</h2>
+
+      <div ref={boardRef} id="board"></div>
+      <div id="settings">
+        <button id="reset" onClick={resetBoard}>
+          Reset
+        </button>
+        <div className="slider-container">
+        <label htmlFor="grid-size-slider">Grid Size: {size} x {size}</label>
+        <input
+          id="grid-size-slider"
+          type="range"
+          min="4"
+          max="64"
+          value={size}
+          onChange={handleSliderChange}
+        />
+        </div>
+
+      </div>
     </div>
   );
 }
